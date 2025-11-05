633c9dec7a6a3bda90072780ccf5575503cb6064
shanelwilliams <shanelwilliams2018@gmail.com>
2025-11-05 08:48:37 -0700

    variables practice

---
+++ script.js
@@ -0,0 +1,48 @@
+// Ways to declare a variable
+//let, const 
+//let can change, const has to be the same variable and cannot change
+
+let firstName = 'John'
+let lastName = 'Doe'
+
+console.log(firstName, lastName)
+
+let age = 30
+
+console.log(age)
+
+// Naming conventions
+// - Only letters, numbers, underscores and dollar signs
+// - Can't start with a number
+
+/* Multi-Word Formatting
+firstName = camelCase  *normal
+first_name = underscore
+FirstName =  PascalCase
+firstname = lowercase*/
+
+// Re-assigning Variables
+
+age = 31
+console.log(age)
+
+let score
+
+score = 1
+
+console.log(score)
+
+if (true) {
+    score = score + 1
+}
+
+console.log(score)
+
+const x = 100
+
+const arr = [1,2,3,4]
+
+arr.push(5)
+
+console.log(arr)
+
