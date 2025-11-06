cf96f1813ac58b21da8de525219d8893bd529f8f
shanelwilliams <shanelwilliams2018@gmail.com>
2025-11-06 14:17:09 -0700

    Went over strings

---
+++ strings/script.js
@@ -0,0 +1,24 @@
+let x
+
+const name = 'John'
+const age = 31
+
+x = 'Hello, my name is ' + name + ' and I am ' + age + ' years old.'
+
+// Template literals
+x = `Hello, my name is ${name} and I am ${age} years old.`
+
+// String properties and Methods
+const s = new String('Hello World')
+
+x = typeof s
+
+x = s.length
+
+// Access value by key
+x = s[1]
+
+x = s.__proto__
+
+
+console.log(x)
